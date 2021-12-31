3e12ee1ff8b6e6097a60cdf1ca3b4085011c28cb
gavincotterill <gvncotterill@gmail.com>
2021-12-31 13:45:27 -0600

    I think there was just a missing set of parentheses on line 85 that was forcing the number of samples to always be 1 per day

--- R/simulate_animal.R
+++ R/simulate_animal.R
@@ -82,7 +82,7 @@ simulate_animal <- function(time_to_leave_group,
   locations$at_home <- ifelse(locations$current_state == animals_home, "home", "away")
 
   # sample group membership on a fixed time grid and record ----
-  sample_times <- seq(1:sampling_duration * samples_per_day) / samples_per_day # should this be divided?
+  sample_times <- seq(1:(sampling_duration * samples_per_day)) / samples_per_day # added parentheses around sampling dur * spd
   sample_locations <- rep(NA, length(sample_times))
   for(i in 1:length(sample_times)){
     sample_locations[i] <- locations$current_state[min(which(locations$cumulative_time >= sample_times[i]))]
