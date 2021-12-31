12c029d48c18a8de8be81f7e0471ab8f4cfa0bfe
gavincotterill <gvncotterill@gmail.com>
2021-12-31 14:20:06 -0600

    made example more minimal added seealso for plot_simulated_graph

--- R/simulate_graph.R
+++ R/simulate_graph.R
@@ -24,44 +24,7 @@
 #'                     samples_per_day = 1,
 #'                     sampling_duration = 7,
 #'                     time_cut = 7)
-#'
-#' igraph::V(g)$name <- stringr::str_extract(igraph::V(g)$name, "\\d{1,}")
-#'
-#' grp <- data.frame(name = igraph::V(g)$name,
-#'                   mem = igraph::V(g)$membership)
-#'
-#' mark_col <- grDevices::rainbow(length(unique(grp$mem)), alpha = 0.3)
-#' mark_border <- grDevices::rainbow(length(unique(grp$mem)), alpha = 1)
-#'
-#' grp$mc <- mark_col[grp$mem]
-#' grp$mb <- mark_border[grp$mem]
-#'
-#' whole_lists <- grp %>%
-#'   dplyr::group_by(mem) %>%
-#'   tidyr::nest() %>%
-#'   dplyr::mutate(data = map(data, as.list)) %>%
-#'   dplyr::pull(data)
-#'
-#' grp_list_whole <- lapply(whole_lists, `[[`, "name")
-#' mcs_whole <- lapply(whole_lists, `[[`, "mc") %>%
-#'   purrr::flatten() %>%
-#'   unique() %>%
-#'   unlist()
-#' mbs_whole <- lapply(whole_lists, `[[`, "mb") %>%
-#'   purrr::flatten() %>%
-#'   unique() %>%
-#'   unlist()
-#'
-#' igraph:::plot.igraph(g,
-#'                      layout = igraph::layout.fruchterman.reingold(g),
-#'                      edge.width = igraph::E(g)$weight,
-#'                      edge.color = "black",
-#'                      vertex.color = grp$mb,
-#'                      vertex.frame.color = "grey20",
-#'                      mark.groups = grp_list_whole,
-#'                      mark.col = mcs_whole,
-#'                      mark.border = mbs_whole)
-#'
+#' @seealso \code{\link{plot_simulated_graph}}
 simulate_graph <- function(n_animals,
                            n_groups,
                            time_to_leave,
