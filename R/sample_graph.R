c1ca9ce97742040fe0897fc36c45f0111c714556
gavincotterill <gvncotterill@gmail.com>
2021-12-30 22:35:12 -0600

    the 'better' sampler is returning twice the number of nodes it should, not sure why

--- R/sample_graph.R
+++ R/sample_graph.R
@@ -54,7 +54,7 @@ sample_graph <- function(graph, missingness, propGPS = 1, gps_freq = 30/365, vhf
       initial_sampling <- id_df %>%
         dplyr::group_by(group) %>%
         dplyr::sample_n(ifelse(min_sample == 1, 1, 2)) %>%
-        dplyr::slice(1:size)
+        dplyr::slice(1:(size - 1))
 
       remainder <- size - nrow(initial_sampling)
 
