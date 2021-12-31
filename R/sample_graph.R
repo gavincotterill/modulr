0097690749fe75b3f0992607e4a49f52ef222764
gavincotterill <gvncotterill@gmail.com>
2021-12-30 21:55:13 -0600

    removed trycatch from even sampling regime. removed duplicate code between random and even.

--- R/sample_graph.R
+++ R/sample_graph.R
@@ -1,7 +1,5 @@
 #' Simulate a sampling regime and calculate network modularity.
 #'
-#' @param adjmat I'm not sure why this is here... should just build it from the
-#'   graph
 #' @param graph The true network to sample
 #' @param missingness The proportion of the nodes in the real network that will
 #'   be missed in sampling
@@ -9,7 +7,7 @@
 #' @param gps_freq The observation frequency (observations per sampling period)
 #'   of the high res data
 #' @param vhf_freq Observations per sampling period of the low res data
-#' @param regime Whether to sample modules randomly or impose 'evenness'
+#' @param regime Whether to sample modules randomly or impose 'evenness'. options include "random", "even", "better"
 #'
 #' @return g_obs, the graph of the observed network
 #' @export
@@ -43,7 +41,7 @@ sample_graph <- function(graph, missingness, propGPS, gps_freq, vhf_freq, regime
     size = ceiling( (1 - m) * netSize ) # the number of animals to return
     even_sampler <- floor(size/ngroups)
 
-    tryCatch({
+
       if(size > ngroups & even_sampler >= min_sample){ # start sampling if
 
         initial_sampling <- id_df %>%
@@ -78,122 +76,68 @@ sample_graph <- function(graph, missingness, propGPS, gps_freq, vhf_freq, regime
         out <- sampling[1:size,]
       } # end sampling if()
 
-      if(exists("out")){ # start exists out
-        grab <- out$ids
-        if(length(grab) > 2){ # start grab if()
-          am <- adjmat[which(rownames(adjmat) %in% grab), which(colnames(adjmat) %in% grab)]
-          g2 <- igraph::graph_from_adjacency_matrix(am, weighted = T, mode = "undirected", diag = F)
-          ed <- igraph::get.data.frame(g2) # weights are all super small when they're each in a different module
+    grab <- out$ids
 
-          if(nrow(ed) > 1){
-            nNodes_sim <- length(grab)
-            nGPS <- ceiling(nNodes_sim * propGPS)
-            nVHF <- nNodes_sim - nGPS
-
-            ofd <- data.frame(id = row.names(am)) %>%
-              dplyr::mutate(obs_freq = ifelse(as.numeric(row.names(.)) <= nGPS, gps_freq, vhf_freq),
-                     n_obs = obs_freq * 365)
-            ed$fromObs <- ofd$n_obs[match(ed$from, ofd$id)]
-            ed$toObs <- ofd$n_obs[match(ed$to, ofd$id)]
-            ed$minObs <- pmin(ed$fromObs, ed$toObs)
-            ed$sim_weight <- stats::rbinom(n = nrow(ed), size = ed$minObs, p = ed$weight) / ed$minObs # when they're all in diff mods, with low weights, simweight has a lot of zeros
-
-            g_obs <- igraph::graph_from_data_frame(ed[,c("from", "to", "sim_weight")], directed = FALSE)
-            g_obs <- igraph::delete_edges(g_obs, which(igraph::E(g_obs)$sim_weight==0))
-            am_obs <- igraph::get.adjacency(g_obs, type = "upper", attr = "sim_weight") %>% as.matrix()
-
-            if( length( igraph::E(g_obs) ) >= 2 ){
-
-              df <- rnetcarto::netcarto(am_obs)[[1]]
-              nms <- igraph::V(g_obs)$name
-              nmsNC <- df$name
-              if(all(nms %in% nmsNC)){
-                (colorOrder <- df$module[match(nms, nmsNC)])
-              }else{
-                colorOrder <- df$module[match(nms, nmsNC)]
-                for(j in 1:sum(is.na(colorOrder))){
-                  colorOrder[is.na(colorOrder)][1] <- max(colorOrder, na.rm = T) + 1 # this is counterintuitive... but use a [1] instead of [j]
-                }
-              }
-              igraph::V(g_obs)$membership <- colorOrder
-
-            }else if(length(igraph::E(g_obs)) == 1){
-
-              head <- names(which(rowSums(am_obs) > 0))
-              tail <- names(which(colSums(am_obs) > 0))
-              colorOrder <- ifelse(igraph::V(g_obs)$name %in% c(head, tail), 0, NA)
-              for(j in 1:sum(is.na(colorOrder))){
-                colorOrder[is.na(colorOrder)][1] <- max(colorOrder, na.rm = T) + 1 # this is counterintuitive... but use a [1] instead of [j]
-              }
-              igraph::V(g_obs)$membership <- colorOrder
-
-            }else if(length(igraph::E(g_obs)) == 0){
-
-              igraph::V(g_obs)$membership <- 1:length(igraph::V(g_obs))
-
-            }
-            return(g_obs)
-          }
-        }
-      }
-    }, error = function(e){print(paste("i, j, k = ", i, j, k, sep = "  "))} )
-  }
+    }
 
   if(regime == "random"){
+
     grab <- sample(1:netSize, size = ceiling( (1 - m) * netSize ), replace = F)
-    if(length(grab) > 2){
-      am <- adjmat[grab, grab]
-      g2 <- igraph::graph_from_adjacency_matrix(am, weighted = T, mode = "undirected", diag = F)
-      ed <- igraph::get.data.frame(g2)
 
-      nNodes_sim <- length(grab)
-      nGPS <- ceiling(nNodes_sim * propGPS)
-      nVHF <- nNodes_sim - nGPS
+    }
 
-      ofd <- data.frame(id = row.names(am)) %>%
-        dplyr::mutate(obs_freq = ifelse(as.numeric(row.names(.)) <= nGPS, gps_freq, vhf_freq),
-               n_obs = obs_freq * 365)
-      ed$fromObs <- ofd$n_obs[match(ed$from, ofd$id)]
-      ed$toObs <- ofd$n_obs[match(ed$to, ofd$id)]
-      ed$minObs <- pmin(ed$fromObs, ed$toObs)
-      ed$sim_weight <- stats::rbinom(n = nrow(ed), size = ed$minObs, p = ed$weight) / ed$minObs
+  if(length(grab) > 2){
+    am <- adjmat[grab, grab]
+    g2 <- igraph::graph_from_adjacency_matrix(am, weighted = T, mode = "undirected", diag = F)
+    ed <- igraph::get.data.frame(g2)
 
-      g_obs <- igraph::graph_from_data_frame(ed[,c("from", "to", "sim_weight")], directed = FALSE)
-      g_obs <- igraph::delete_edges(g_obs, which(igraph::E(g_obs)$sim_weight==0))
-      am_obs <- igraph::get.adjacency(g_obs, type = "upper", attr = "sim_weight") %>% as.matrix()
+    nNodes_sim <- length(grab)
+    nGPS <- ceiling(nNodes_sim * propGPS)
+    nVHF <- nNodes_sim - nGPS
 
-      if( length( igraph::E(g_obs) ) >= 2 ){
+    ofd <- data.frame(id = row.names(am)) %>%
+      dplyr::mutate(obs_freq = ifelse(as.numeric(row.names(.)) <= nGPS, gps_freq, vhf_freq),
+             n_obs = obs_freq * 365)
+    ed$fromObs <- ofd$n_obs[match(ed$from, ofd$id)]
+    ed$toObs <- ofd$n_obs[match(ed$to, ofd$id)]
+    ed$minObs <- pmin(ed$fromObs, ed$toObs)
+    ed$sim_weight <- stats::rbinom(n = nrow(ed), size = ed$minObs, p = ed$weight) / ed$minObs
 
-        df <- rnetcarto::netcarto(am_obs)[[1]]
-        nms <- igraph::V(g_obs)$name
-        nmsNC <- df$name
-        if(all(nms %in% nmsNC)){
-          (colorOrder <- df$module[match(nms, nmsNC)])
-        }else{
-          colorOrder <- df$module[match(nms, nmsNC)]
-          for(j in 1:sum(is.na(colorOrder))){
-            colorOrder[is.na(colorOrder)][1] <- max(colorOrder, na.rm = T) + 1 # this is counterintuitive... but use a [1] instead of [j]
-          }
-        }
-        igraph::V(g_obs)$membership <- colorOrder
+    g_obs <- igraph::graph_from_data_frame(ed[,c("from", "to", "sim_weight")], directed = FALSE)
+    g_obs <- igraph::delete_edges(g_obs, which(igraph::E(g_obs)$sim_weight==0))
+    am_obs <- igraph::get.adjacency(g_obs, type = "upper", attr = "sim_weight") %>% as.matrix()
 
-      }else if(length(igraph::E(g_obs)) == 1){
+    if( length( igraph::E(g_obs) ) >= 2 ){
 
-        head <- names(which(rowSums(am_obs) > 0))
-        tail <- names(which(colSums(am_obs) > 0))
-        colorOrder <- ifelse(igraph::V(g_obs)$name %in% c(head, tail), 0, NA)
+      df <- rnetcarto::netcarto(am_obs)[[1]]
+      nms <- igraph::V(g_obs)$name
+      nmsNC <- df$name
+      if(all(nms %in% nmsNC)){
+        (colorOrder <- df$module[match(nms, nmsNC)])
+      }else{
+        colorOrder <- df$module[match(nms, nmsNC)]
         for(j in 1:sum(is.na(colorOrder))){
           colorOrder[is.na(colorOrder)][1] <- max(colorOrder, na.rm = T) + 1 # this is counterintuitive... but use a [1] instead of [j]
         }
-        igraph::V(g_obs)$membership <- colorOrder
-
-      }else if(length(igraph::E(g_obs)) == 0){
-
-        igraph::V(g_obs)$membership <- 1:length(igraph::V(g_obs))
-
       }
-      return(g_obs)
+      igraph::V(g_obs)$membership <- colorOrder
+
+    }else if(length(igraph::E(g_obs)) == 1){
+
+      head <- names(which(rowSums(am_obs) > 0))
+      tail <- names(which(colSums(am_obs) > 0))
+      colorOrder <- ifelse(igraph::V(g_obs)$name %in% c(head, tail), 0, NA)
+      for(j in 1:sum(is.na(colorOrder))){
+        colorOrder[is.na(colorOrder)][1] <- max(colorOrder, na.rm = T) + 1 # this is counterintuitive... but use a [1] instead of [j]
+      }
+      igraph::V(g_obs)$membership <- colorOrder
+
+    }else if(length(igraph::E(g_obs)) == 0){
+
+      igraph::V(g_obs)$membership <- 1:length(igraph::V(g_obs))
+
     }
+    return(g_obs)
   }
 
 }
