21bfc0cde5eb497f2343181c58d3dfa12b4a128e
gavincotterill <gvncotterill@gmail.com>
2022-02-11 09:26:27 -0600

    added instantaneous monitoring of group composition, although it is slow. new param = sampler = c(discrete or continuous)

--- R/simulate_graph.R
+++ R/simulate_graph.R
@@ -5,8 +5,13 @@
 #' @param time_to_leave The average number of days spent in the home group.
 #' @param time_to_return The average number of days spent away from the home
 #'   group.
-#' @param samples_per_day The number of samples per day.
 #' @param sampling_duration The number of days in the sampling period.
+#' @param sampler Takes one of c("discrete", "continuous"). Whether group
+#' composition should be monitored instantaneously (truth), or sampled in
+#' discrete time. Continuous monitoring is much slower.
+#' @param samples_per_day Optional. If sampler == "discrete", the number of
+#' samples taken per day in order to estimate the simple ratio index for each
+#' dyad.
 #'
 #' @return sim_igraph, an igraph graph object.
 #' @export
@@ -18,19 +23,28 @@
 #'                     n_groups = 4,
 #'                     time_to_leave = 5,
 #'                     time_to_return = 2,
-#'                     samples_per_day = 1,
-#'                     sampling_duration = 7)
+#'                     sampling_duration = 7,
+#'                     sampler = "discrete",
+#'                     samples_per_day = 1
+#'                     )
 #' @seealso \code{\link{plot_simulated_graph}}
 simulate_graph <- function(n_animals,
                            n_groups,
                            time_to_leave,
                            time_to_return,
-                           samples_per_day,
-                           sampling_duration) {
+                           sampling_duration,
+                           sampler = "discrete",
+                           samples_per_day) {
 
   if (!requireNamespace(c("igraph"), quietly = TRUE)) {
     stop(
       "Package \"igraph\"  must be installed to use this function.",
+      call. = FALSE
+    )
+  }
+  if (!sampler %in% c("discrete", "continuous")) {
+    stop(
+      "sampler must be either \"discrete\" or \"continuous\".",
       call. = FALSE
     )
   }
@@ -59,6 +73,7 @@ simulate_graph <- function(n_animals,
       animal_sample_df[[a]] <- cbind(animal_list[[a]]$samples,
                                      id = rep(paste("Animal_", a, sep = ""), nrow(animal_list[[a]]$samples)))
       names(animal_sample_df)[a] <- (paste0("Animal_", a))
+
     }
   } else if( length(time_to_leave) != length(time_to_return) | !length(time_to_leave) %in% c(1, n_animals) | !length(time_to_return) %in% c(1, n_animals)){
     stop(
@@ -67,32 +82,95 @@ simulate_graph <- function(n_animals,
     )
   }
 
+  if(sampler == "discrete"){
+    mem_df <- data.frame(ids = names(animal_sample_df),
+                         membership = unlist(lapply(animal_list, function(x) x[['animals_home']])))
+    # collect output into a common GPS-like dataframe.
+    samples_out <- as.data.frame(do.call("rbind", animal_sample_df))
+    names(samples_out) <- c("time", "location", "id")
 
+    # build SRI adjacency matrix -----
+    dyads <- expand.grid(1:n_animals, 1:n_animals)
+    dyads <- dyads[which(dyads[, 1] != dyads[, 2]), ] # this still includes eg A--B and B--A, but that shouldn't add much time
 
-  mem_df <- data.frame(ids = names(animal_sample_df),
-                       membership = unlist(lapply(animal_list, function(x) x[['animals_home']])))
+    adj_mat <- matrix(NA, nrow = n_animals,ncol = n_animals)
 
-  # collect output into a common GPS-like dataframe.
-  samples_out <- as.data.frame(do.call("rbind", animal_sample_df))
-  names(samples_out) <- c("time", "location", "id")
+    for(d in 1:nrow(dyads)){
+      anim1 <- subset(samples_out, id == levels(factor(samples_out$id))[dyads[d, 1]])
+      anim2 <- subset(samples_out, id == levels(factor(samples_out$id))[dyads[d, 2]])
+      together <- length(which(anim1$location == anim2$location))
+      adj_mat[dyads[d, 1], dyads[d, 2]] <- together / nrow(anim1)
+    }
 
-  # build SRI adjacency matrix -----
-  dyads <- expand.grid(1:n_animals, 1:n_animals)
-  dyads <- dyads[which(dyads[, 1] != dyads[, 2]), ] # this still includes eg A--B and B--A, but that shouldn't add much time
-
-  adj_mat <- matrix(NA, nrow = n_animals,ncol = n_animals)
-
-  for(d in 1:nrow(dyads)){
-    anim1 <- subset(samples_out, id == levels(factor(samples_out$id))[dyads[d, 1]])
-    anim2 <- subset(samples_out, id == levels(factor(samples_out$id))[dyads[d, 2]])
-    ### Currently SRI is not continuous-- based on sample frequency, not instantaneous:
-    together <- length(which(anim1$location == anim2$location))
-    adj_mat[dyads[d, 1], dyads[d, 2]] <- together / nrow(anim1)
+    rownames(adj_mat) <- colnames(adj_mat) <- levels(factor(samples_out$id))
+    diag(adj_mat) <- rep(0, n_animals) # zero diagonal and lower tri
+    adj_mat[lower.tri(adj_mat)] <- 0
   }
 
-  rownames(adj_mat) <- colnames(adj_mat) <- levels(factor(samples_out$id))
-  diag(adj_mat) <- rep(0, n_animals) # zero diagonal and lower tri
-  adj_mat[lower.tri(adj_mat)] <- 0
+
+  if(sampler == "continuous"){
+    names(animal_list) <- 1:length(animal_list)
+
+    mem_df <- data.frame(ids = names(animal_list),
+                         membership = unlist(lapply(animal_list, function(x) x[['animals_home']])))
+
+    # build SRI adjacency matrix -----
+    dyads <- expand.grid(1:n_animals, 1:n_animals)
+    dyads <- dyads[which(dyads[, 1] != dyads[, 2]), ] # this still includes eg A--B and B--A, but that shouldn't add much time
+
+    dt_fxn <- function(animal){
+      one <- animal
+      t1 <- one$locations %>%
+        dplyr::mutate(end = dplyr::lead(cumulative_time),
+                      state = dplyr::lead(current_state)) %>%
+        dplyr::slice(1:(nrow(.)-1)) %>%
+        dplyr::rename(start = cumulative_time) %>%
+        dplyr::select("state", "start","end") %>%
+        data.table::setDT() %>%
+        data.table::setkey(., start, end)
+      t1
+    }
+
+    adj_mat <- matrix(NA, nrow = n_animals,ncol = n_animals)
+
+    for(d in 1:nrow(dyads)){
+
+      a1 <- animal_list[[dyads[d,"Var2"]]]
+      a2 <- animal_list[[dyads[d,"Var1"]]]
+
+      t1 <- dt_fxn(a1)
+      t2 <- dt_fxn(a2)
+
+      intervals <- data.table::foverlaps(t1, t2) %>%
+        dplyr::mutate(start_max = pmax(start, i.start),
+                      end_min = pmin(end, i.end),
+                      together = ifelse(state == i.state, 1, 0))
+
+      g_int <- data.frame(intervals) %>%
+        dplyr::select(1,4,7:9)
+
+      together <- g_int[,c(1,3:5)] %>%
+        dplyr::mutate(id = "A") %>%
+        rbind(g_int[,2:5]  %>% 'names<-'(names(g_int)[c(1,3:5)]) %>% dplyr::mutate(id = "B"))
+
+      time_overlap <- together %>%
+        dplyr::filter(id == "A" & together == 1) %>%
+        dplyr::mutate(time = end_min - start_max)
+
+      (numer <- sum(time_overlap$time))
+      (denom <- pmin(intervals[nrow(intervals),"end"], intervals[nrow(intervals),"i.end"]))
+
+      (edge_weight <- numer / denom )
+
+      dyads$ew[d] <- edge_weight$end
+      adj_mat[dyads[d, 1], dyads[d, 2]] <- dyads$ew[d]
+    }
+
+    rownames(adj_mat) <- colnames(adj_mat) <- names(animal_list)
+    diag(adj_mat) <- rep(0, n_animals) # zero diagonal and lower tri
+    adj_mat[lower.tri(adj_mat)] <- 0
+  }
+
 
   # convert to igraph object and plot with f-r layout
   sim_igraph <- igraph::graph_from_adjacency_matrix(adj_mat, weighted = T, mode = "undirected")
@@ -107,3 +185,5 @@ simulate_graph <- function(n_animals,
   return(sim_igraph)
 
 }
+
+
