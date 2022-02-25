2fea62825b9320c6f152bf0eb459441a7d9ca247
gavincotterill <gvncotterill@gmail.com>
2022-02-24 19:44:01 -0600

    real network doc

---
+++ R/real_networks.R
@@ -0,0 +1,8 @@
+#' Simulated networks with 25 nodes and 2 to 5 modules.
+#'
+#' A list of igraph objects. Time to leave group and time to return to group
+#' take values (5,10) and (1,3,5,7), for all combinations where time to leave >
+#' time to return.
+#'
+#' @format A list of 24 igraph objects.
+"real_networks"
