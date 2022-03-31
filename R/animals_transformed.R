204819f8c0262cf842c1a8975b486e9e8b5a93eb
gavincotterill <gvncotterill@gmail.com>
2022-03-31 13:48:11 -0500

    adding graph crossing

---
+++ R/animals_transformed.R
@@ -0,0 +1,28 @@
+#' simulate group switching to get animals_transformed df
+#' @inheritParams simulate_groups
+#'
+animals_transformed <- function(n_animals,
+                                n_groups,
+                                time_to_leave,
+                                time_to_return,
+                                travel_time,
+                                sampling_duration) {
+  animal_list <- animal_sample_df <- vector("list", length = n_animals)
+  for(a in 1:n_animals){
+    animal_list[[a]] <- modulr::simulate_animal(n_groups = n_groups,
+                                                time_to_leave = time_to_leave,
+                                                time_to_return = time_to_return,
+                                                travel_time = travel_time,
+                                                sampling_duration = sampling_duration)
+    animal_sample_df[[a]] <- cbind(animal_list[[a]]$samples,
+                                   id = rep(paste("Animal_", a, sep = ""), nrow(animal_list[[a]]$samples)))
+    names(animal_sample_df)[a] <- (paste0("Animal_", a))
+  }
+  names(animal_list) <- 1:length(animal_list)
+  animals_transformed <- lapply(animal_list, dt_fxn)
+  return(animals_transformed)
+}
+
+
+
+
---
+++ R/bolts.R
@@ -0,0 +1,19 @@
+#' internal function to graph crossing
+#' @params animal
+#' @params infectious
+bolts <- function(animal, infectious){
+  new_overlaps <- data.frame()
+  for(i in 1:nrow(infectious)){
+    mod <- as.numeric(infectious[i, "state"])
+    x <- dplyr::filter(animal, state == mod)
+    y <- data.table::setkey(infectious[i,2:3])
+
+    df <- na.omit(data.table::foverlaps(x,y)) %>%
+      dplyr::mutate(start = ifelse(i.start > start, i.start, start),
+                    end = ifelse(i.end < end, i.end, end)) %>%
+      dplyr::select(state, start, end)
+
+    new_overlaps <- rbind(new_overlaps, df)
+  }
+  return(new_overlaps)
+}
