1314c79ddd13b1ff209b131f48aa30f3d41832a2
gavincotterill <gvncotterill@gmail.com>
2022-04-05 15:40:35 -0500

    moved idx into function so it doesnt have to be defined. tried to add an error handler on if length(out) > 1, but due to scoping, it probably wouldnn't print to console anyway? not sure if its needed, seems to be working fine.

--- R/index_forward.R
+++ R/index_forward.R
@@ -1,12 +1,15 @@
 #' Index forward
 #' @inheritParams index_back
 #' @keywords internal
-index_forward <- function(df, column_name, value, i, idx = t2$idx){
+index_forward <- function(df, column_name, value, i){
+  idx <- df$idx
+# index_forward <- function(df, column_name, value, i, idx = t2$idx){
   (w <- df[idx == idx[i]+1,])
   # (value_rows <- stringr::str_which(df[, column_name], value))
   (value_rows <- grep(paste0("\\b",value,"\\b"), df[,column_name]))
-  value_rows[value_rows %in% rownames(w)]
+
+  out <- value_rows[value_rows %in% rownames(w)]
   # value_rows[min(which(value_rows > i))]
   # value_rows[min(which(value_rows > i) & which(idx == idx[i] + 1)) ]
-
+  if(length(out) > 1){stop(print(i))}else{return(out)}
 }
