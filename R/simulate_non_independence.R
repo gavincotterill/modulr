80f3efdab11a7cf38be2d3d3042b3645b40aa2e9
gavincotterill <gvncotterill@gmail.com>
2022-04-05 15:40:48 -0500

    added package names

--- R/simulate_non_independence.R
+++ R/simulate_non_independence.R
@@ -83,7 +83,7 @@ simulate_non_independence <- function(
   #--------------------------------------
   # set.seed(123)
   for(i in 1:nrow(t2)){
-    # if(i == 239){break}
+    # if(i == 114){break}
     if(t2$action[i] %in% c(NA)){
       next
     }else if(t2$action[i] == "same"){
@@ -91,14 +91,14 @@ simulate_non_independence <- function(
       if(is.na(t2$members[i])){
         t2$members[i] <- t2$members[index_back_same(t2, "vector", t2$vector[i], i)]
       }else{
-        (grp_tags <- str_extract_all(t2$members[i], "\\d{1,}(?=_)")[[1]] %>% unique())
-        (incoming_grps <- str_split(t2$vector[i], "-")[[1]])
-        (incoming_ids <- str_split(t2$members[index_back_same(t2, "vector", t2$vector[i], i)], "/")[[1]])
-        (existing_mems <- str_split(t2$members[i], "/")[[1]])
+        (grp_tags <- stringr::str_extract_all(t2$members[i], "\\d{1,}(?=_)")[[1]] %>% unique())
+        (incoming_grps <- stringr::str_split(t2$vector[i], "-")[[1]])
+        (incoming_ids <- stringr::str_split(t2$members[index_back_same(t2, "vector", t2$vector[i], i)], "/")[[1]])
+        (existing_mems <- stringr::str_split(t2$members[i], "/")[[1]])
         for(k in seq_along(incoming_grps)){
           if(incoming_grps[k] %in% grp_tags){
             grp_to_paste <- grp_tags[which(grp_tags == incoming_grps[k])]
-            ids_to_paste <- existing_mems[str_detect(existing_mems, paste0(grp_to_paste,"_"))]
+            ids_to_paste <- existing_mems[stringr::str_detect(existing_mems, paste0(grp_to_paste,"_"))]
             incoming_ids[k] <- paste(incoming_ids[k], ids_to_paste, sep = "-")
           }
         }
@@ -107,7 +107,7 @@ simulate_non_independence <- function(
       # look forward
       if(t2$start[i] == max(t2$start)){next}
       curr_vec <- stringr::str_split(t2$vector[i], "-")[[1]]
-      mbrs_list <- as.list(str_split(t2$members[i], "/")[[1]]) %>% `names<-`(curr_vec)
+      mbrs_list <- as.list(stringr::str_split(t2$members[i], "/")[[1]]) %>% `names<-`(curr_vec)
       t2 <- ff_forward2(t2, curr_vec, mbrs_list, i, time_to_leave, time_to_return)
     }else if(t2$action[i] == "fusion"){
       # take care of current, but these will also be used looking forward
@@ -115,8 +115,8 @@ simulate_non_independence <- function(
       n <- length(curr_vec)
       mbrs_list <- list()
       for(j in 1:n){
-        all_mems_back <- str_split(t2$members[index_back(t2, "vector", curr_vec[j], i)] , "/")[[1]]
-        vec_back <- str_split(t2$vector[index_back(t2, "vector", curr_vec[j], i)] , "-")[[1]]
+        all_mems_back <- stringr::str_split(t2$members[index_back(t2, "vector", curr_vec[j], i)] , "/")[[1]]
+        vec_back <- stringr::str_split(t2$vector[index_back(t2, "vector", curr_vec[j], i)] , "-")[[1]]
         mem_idx <- which(vec_back == curr_vec[j])
         mbrs_list[[j]] <- all_mems_back[mem_idx]
         names(mbrs_list)[j] <- curr_vec[[j]]
@@ -124,14 +124,14 @@ simulate_non_independence <- function(
       if(is.na(t2$members[i])){
         t2$members[i] <- paste(unlist(mbrs_list), collapse = "/") # assign current
       }else{
-        (grp_tags <- str_extract_all(t2$members[i], "\\d{1,}(?=_)")[[1]] %>% unique())
-        (incoming_grps <- str_split(t2$vector[i], "-")[[1]])
+        (grp_tags <- stringr::str_extract_all(t2$members[i], "\\d{1,}(?=_)")[[1]] %>% unique())
+        (incoming_grps <- stringr::str_split(t2$vector[i], "-")[[1]])
         (incoming_ids <- paste(mbrs_list, sep = " "))
-        (existing_mems <- str_split(t2$members[i], "/")[[1]])
+        (existing_mems <- stringr::str_split(t2$members[i], "/")[[1]])
         for(k in seq_along(incoming_grps)){
           if(incoming_grps[k] %in% grp_tags){
             grp_to_paste <- grp_tags[which(grp_tags == incoming_grps[k])]
-            ids_to_paste <- existing_mems[str_detect(existing_mems, paste0(grp_to_paste,"_"))]
+            ids_to_paste <- existing_mems[stringr::str_detect(existing_mems, paste0(grp_to_paste,"_"))]
             incoming_ids[k] <- paste(incoming_ids[k], ids_to_paste, sep = "-")
           }
         }
@@ -139,7 +139,7 @@ simulate_non_independence <- function(
       }
       # look forward
       if(t2$start[i] == max(t2$start)){next}
-      mbrs_list <- as.list(str_split(t2$members[i], "/")[[1]]) %>% `names<-`(curr_vec)
+      mbrs_list <- as.list(stringr::str_split(t2$members[i], "/")[[1]]) %>% `names<-`(curr_vec)
       t2 <- ff_forward2(t2, curr_vec, mbrs_list, i, time_to_leave, time_to_return)
     } else if(t2$action[i] %in% c("fission", "fission-fusion")){
       # members should always be accounted for to start
@@ -147,11 +147,10 @@ simulate_non_independence <- function(
       # look forward
       if(t2$start[i] == max(t2$start)){next}
       curr_vec <- stringr::str_split(t2$vector[i], "-")[[1]]
-      mbrs_list <- as.list(str_split(t2$members[i], "/")[[1]]) %>% `names<-`(curr_vec)
+      mbrs_list <- as.list(stringr::str_split(t2$members[i], "/")[[1]]) %>% `names<-`(curr_vec)
       t2 <- ff_forward2(t2, curr_vec, mbrs_list, i, time_to_leave, time_to_return)
     }
   }
-
   return(t2)
 }
 
