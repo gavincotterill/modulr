9fc5eb1711e6fa84bc469ac55ed375fbecb101e1
gavincotterill <gvncotterill@gmail.com>
2022-05-03 09:33:25 -0500

    minor changes to annotations

--- R/delta_grp.R
+++ R/delta_grp.R
@@ -3,7 +3,6 @@
 #' @keywords internal
 delta_grp <- function(df, column_name, value, i){
   out <- tryCatch({
-    # if(stringr::str_length(df[i, column_name]) > 1){
     if(length(stringr::str_split(df[i, column_name], "-")[[1]]) > 1){
       vals <- stringr::str_split(df[i, column_name], "-")[[1]]
       find_vals <- list()
@@ -11,30 +10,22 @@ delta_grp <- function(df, column_name, value, i){
       prev_vals <- list()
       for(j in seq_along(vals)){
         find_vals[j] <- vals[[j]]
-        # prev_ocrs[[j]] <- grep(find_vals[[j]], df[,column_name])
-        prev_ocrs[[j]] <- grep(paste0("\\b", find_vals[[j]], "\\b"), df[,column_name])
+        prev_ocrs[[j]] <- grep(paste0("\\b", find_vals[[j]], "\\b"), df[,column_name]) # I don't know why I'm not using index_back here
         prev_vals[j] <- df[prev_ocrs[[j]][max(which(prev_ocrs[[j]] < i))], column_name]
       }
       unique_prev_val <- unique(prev_vals)
-      # prev_val <- stringr::str_split(prev_val, "-")
       prev_val <- stringr::str_split(unique_prev_val, "-")
-
       curr_val <- stringr::str_split(df[i, column_name], "-")[[1]]
       if(length(unlist(prev_val)) > length(curr_val) & any(!curr_val %in% prev_val[[1]])){ # if there were more before, and any unlisted current were not in listed previous
         "fission-fusion"
       }else if(length(unlist(prev_val)) > length(curr_val) & all(curr_val%in% prev_val[[1]])){ # if there were more before,  and all current were in listed previous
         "fission"
-        # }else if(length(unlist(prev_val)) <= length(curr_val) & all(curr_val %in% prev_val)){
-        # }else if(length(unlist(prev_val)) < length(curr_val)){
       }else if(length(unlist(prev_val)) <= length(curr_val) & all(curr_val %in% unlist(prev_val)) & !identical(prev_val[[1]], curr_val)){
         "fusion"
-        # }else if(length(unlist(prev_val)) == length(curr_val) & all(curr_val %in% unlist(prev_val))){
       }else if(identical(prev_val[[1]], curr_val)){
         "same"
       }
-      # }else if(stringr::str_length(df[i, column_name]) == 1){
     }else if(length(stringr::str_split(df[i, column_name], "-")[[1]]) == 1){
-      # prev_ocr <- grep(value, df[,column_name])
       prev_ocr <- grep(paste0("\\b", value, "\\b"), df[,column_name])
       prev_val <- df[prev_ocr[max(which(prev_ocr < i))], column_name]
       curr_val <- df[i, column_name]
