cf608f6cddd2dfe767299206c978935ba6184daf
gavincotterill <gvncotterill@gmail.com>
2022-06-28 14:58:31 -0500

    fixing a broken test

--- tests/testthat/test-2.R
+++ tests/testthat/test-2.R
@@ -91,14 +91,14 @@ test_that("simulate_non_independence2 has all individuals at all intervals", {
   expect_silent(plot_non_independence(g))
 })
 
-test_that("graph_from_at produces a graph", {
+test_that("graph_from_schedule produces a graph", {
   at <- simulate_independence(n_animals = 12,
                             n_groups = 2,
                             time_to_leave = 3,
                             time_to_return = 1,
                             travel_time = c(0.001, 1),
                             sampling_duration = 7)
-  g <- graph_from_at(at[[1]], at[[2]])
+  g <- graph_from_schedule(at)
   expect_equal(is(g, "igraph"), TRUE)
 })
 
