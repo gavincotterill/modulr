1342303fe737d8985cf329a2802226ef6d6d99f0
gavincotterill <gvncotterill@gmail.com>
2022-06-28 10:29:22 -0500

    deleting rejoin_animals and remove_animals they aren't used in ff_forward3, they were used in previous versions like ff_forward and ff_forward2

--- R/remove_animals.R
+++
@@ -1,9 +0,0 @@
-#' Deletes animals from a nested list
-#' @param inlist a list like mbrs_list
-#' @param elem_remove a list with same length  as mbrs_list of animals to delete
-#' @keywords internal
-remove_animals <- function(inlist, elem_remove){
-  outlist = lapply(inlist, setdiff, elem_remove)
-  # outlist[lengths(outlist) > 0]
-  outlist
-}
