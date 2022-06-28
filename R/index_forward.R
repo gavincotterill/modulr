b69ef0637f06896dd09acb4d5c572770ebbe7cc0
gavincotterill <gvncotterill@gmail.com>
2022-06-28 12:39:05 -0500

    getting ready to make this branch the main branch

--- R/index_forward.R
+++ R/index_forward.R
@@ -3,15 +3,9 @@
 #' @keywords internal
 index_forward <- function(df, column_name, value, i){
   idx <- df$idx
-# index_forward <- function(df, column_name, value, i, idx = t2$idx){
-  (w <- df[idx == idx[i]+1,])
-  # (value_rows <- stringr::str_which(df[, column_name], value))
-  (value_rows <- grep(paste0("\\b",value,"\\b"), df[,column_name]))
-
+  w <- df[idx == idx[i]+1,]
+  value_rows <- grep(paste0("\\b",value,"\\b"), df[,column_name]) # grep seems to be a little faster
+  # value_rows <- stringr::str_which(df[,column_name], paste0("\\b",value,"\\b"))
   out <- value_rows[value_rows %in% rownames(w)]
-  # value_rows[min(which(value_rows > i))]
-  # value_rows[min(which(value_rows > i) & which(idx == idx[i] + 1)) ]
-  # if(length(out) > 1){stop(print(i))}else{return(out)}
   return(out)
-
 }
--- tests/testthat/test-2.R
+++ tests/testthat/test-2.R
@@ -1,8 +1,8 @@
 library(testthat)
 library(modulr)
 
-test_that("simulate_animal creates list", {
-  at <- animals_transformed(n_animals = 12,
+test_that("simulate_independence creates list", {
+  at <- simulate_independence(n_animals = 12,
                             n_groups = 2,
                             time_to_leave = 3,
                             time_to_return = 1,
@@ -13,8 +13,8 @@ test_that("simulate_animal creates list", {
   expect_output(str(at), "List of 2")
 })
 
-test_that("graph_crossing using first element of animals_transformed output", {
-  at <- animals_transformed(n_animals = 12,
+test_that("graph_crossing using first element of simulate_independence output", {
+  at <- simulate_independence(n_animals = 12,
                             n_groups = 2,
                             time_to_leave = 3,
                             time_to_return = 1,
@@ -49,8 +49,31 @@ test_that("simulate_non_independence has all individuals at all intervals", {
                                   time_to_leave = 5,
                                   time_to_return = 2,
                                   travel_time = c(0.001,2),
-                                  sampling_duration = 7,
-                                  samples_per_day = 1)
+                                  sampling_duration = 7)
+
+  t3 <- t2 %>%
+    dplyr::group_by(start) %>%
+    dplyr::summarise(mems = paste(members, collapse = "-")) %>%
+    dplyr::mutate(mems = stringr::str_replace_all(mems, "/", "-")) %>%
+    dplyr::mutate(mems = stringr::str_split(mems, "-"))
+  t4 <- lapply(t3$mems, unlist)
+  t5 <- purrr::map(t4, sort)
+  allSame <- function(x) length(unique(x)) == 1
+  t6 <- purrr::map2(t5, "", ~ stringr::str_subset(., .y) )
+
+  expect_equal(allSame(t6), TRUE) # should return true
+
+  g <- graph_from_non_independence(t2)
+  expect_silent(plot_non_independence(g))
+})
+
+test_that("simulate_non_independence2 has all individuals at all intervals", {
+  # this test is probably adequate, but there can be empty characters returned in the list "" from dropping dummy animals
+  t2 <- simulate_non_independence2(n_groups = 4,
+                                  time_to_leave = 5,
+                                  time_to_return = 2,
+                                  travel_time = c(0.001,2),
+                                  sampling_duration = 7)
 
   t3 <- t2 %>%
     dplyr::group_by(start) %>%
@@ -69,7 +92,7 @@ test_that("simulate_non_independence has all individuals at all intervals", {
 })
 
 test_that("graph_from_at produces a graph", {
-  at <- animals_transformed(n_animals = 12,
+  at <- simulate_independence(n_animals = 12,
                             n_groups = 2,
                             time_to_leave = 3,
                             time_to_return = 1,
