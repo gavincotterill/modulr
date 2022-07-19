b0b1095a5091b05cf59a30be65cc2903d63edd6b
gavincotterill <gvncotterill@gmail.com>
2022-07-19 09:21:23 -0500

    group think option handler

--- tests/testthat/test-get-times.R
+++ tests/testthat/test-get-times.R
@@ -15,14 +15,21 @@ test_that("get_times works", {
 
   expect_equal(length(s1), nrow(out))
 
-  s2 <- simulate_schedule(n_animals = na, n_groups = 3, n_splits = NA, time_to_leave = 5, time_to_return = 2, travel_time = c(0.001, 0.002), sampling_duration = 14, simulator = "group-think")
+  s2 <- simulate_schedule(n_animals = na,
+                          n_groups = 3,
+                          n_splits = NA,
+                          time_to_leave = 5,
+                          time_to_return = 2,
+                          travel_time = c(0.001, 0.002),
+                          sampling_duration = 14,
+                          simulator = "group-think")
 
   ids2 <- names(s2)
 
-  out2 <- get_times(schedule = s2[[1]], id = ids2[[1]], simulator = "group-think")
+  out2 <- get_times(schedule = s2[[1]], id = ids2[[1]], simulator = "group-think", option = "co-located")
 
   for (i in 2:length(ids2)) {
-    out2[i, ] <- get_times(s2[[i]], ids2[[i]], simulator = "group-think")
+    out2[i, ] <- get_times(s2[[i]], ids2[[i]], simulator = "group-think", option = "co-located")
   }
 
   expect_equal(length(s2), nrow(out2))
